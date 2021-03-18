929c5b61c48ed37fcdedb57b824fa07295d2eb31
kazuma-fujita <kazuma.fujita.zuma@gmail.com>
2021-03-18 18:55:15 +0900

    座標判定追加

--- lib/map_view.dart
+++ lib/map_view.dart
@@ -37,31 +37,14 @@ class _MapView extends HookWidget {
     // 初期表示座標のMarkerを設定
     final initialMarkers = {
       _initialPosition.timestamp.toString(): Marker(
-        markerId: MarkerId(
-          _initialPosition.timestamp.toString(),
-        ),
+        markerId: MarkerId(_initialPosition.timestamp.toString()),
         position: LatLng(_initialPosition.latitude, _initialPosition.longitude),
-      )
+      ),
     };
     final position = useState<Position>(_initialPosition);
     final markers = useState<Map<String, Marker>>(initialMarkers);
 
-    Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high).then(
-      (Position currentPosition) {
-        // 現在地座標にMarkerを立てる
-        final marker = Marker(
-          markerId: MarkerId(currentPosition.timestamp.toString()),
-          position: LatLng(currentPosition.latitude, currentPosition.longitude),
-        );
-        markers.value.clear();
-        markers.value[currentPosition.timestamp.toString()] = marker;
-        // 現在地座標のstateを更新する
-        position.value = currentPosition;
-        print('position:${position.value}');
-      },
-    );
-
-    // _latLngListener(context);
+    _setCurrentLocation(position, markers);
     _animateCamera(position);
 
     return Scaffold(
@@ -70,14 +53,37 @@ class _MapView extends HookWidget {
         myLocationButtonEnabled: false,
         // 初期表示位置は渋谷駅に設定
         initialCameraPosition: CameraPosition(
-          // target: LatLng(_initialPosition.latitude, _initialPosition.longitude),
-          target: LatLng(position.value.latitude, position.value.longitude),
+          target: LatLng(_initialPosition.latitude, _initialPosition.longitude),
           zoom: 14.4746,
         ),
         onMapCreated: _mapController.complete,
         markers: markers.value.values.toSet(),
       ),
     );
+  }
+
+  Future<void> _setCurrentLocation(ValueNotifier<Position> position,
+      ValueNotifier<Map<String, Marker>> markers) async {
+    final currentPosition = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    );
+
+    const decimalPoint = 3;
+    // 過去の座標と最新の座標の小数点第三位で切り捨てた値を判定
+    if ((position.value.latitude).toStringAsFixed(decimalPoint) !=
+            (currentPosition.latitude).toStringAsFixed(decimalPoint) &&
+        (position.value.longitude).toStringAsFixed(decimalPoint) !=
+            (currentPosition.longitude).toStringAsFixed(decimalPoint)) {
+      // 現在地座標にMarkerを立てる
+      final marker = Marker(
+        markerId: MarkerId(currentPosition.timestamp.toString()),
+        position: LatLng(currentPosition.latitude, currentPosition.longitude),
+      );
+      markers.value.clear();
+      markers.value[currentPosition.timestamp.toString()] = marker;
+      // 現在地座標のstateを更新する
+      position.value = currentPosition;
+    }
   }
 
   Future<void> _animateCamera(ValueNotifier<Position> position) async {
@@ -88,64 +94,5 @@ class _MapView extends HookWidget {
         LatLng(position.value.latitude, position.value.longitude),
       ),
     );
-  }
-
-  Future<void> _latLngListener(BuildContext context) async {
-    final screenWidth = MediaQuery.of(context).size.width *
-        MediaQuery.of(context).devicePixelRatio;
-    final screenHeight = MediaQuery.of(context).size.height *
-        MediaQuery.of(context).devicePixelRatio;
-
-    final middleX = screenWidth / 2;
-    final middleY = screenHeight / 2;
-
-    final screenCoordinate =
-        ScreenCoordinate(x: middleX.round(), y: middleY.round());
-    final mapController = await _mapController.future;
-    final latLng = await mapController.getLatLng(screenCoordinate);
-
-    print('latLng:$latLng');
-  }
-
-  // Future<void> _goToTheLake() async {
-  //   final controller = await _mapController.future;
-  //   await controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
-  // }
-
-  /// Determine the current position of the device.
-  ///
-  /// When the location services are not enabled or permissions
-  /// are denied the `Future` will return an error.
-  Future<Position> _determinePosition() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      return Future.error('Location services are disabled.');
-    }
-
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.deniedForever) {
-        // 設定画面へ遷移
-        // await Geolocator.openAppSettings();
-        // 設定画面へ遷移
-        await Geolocator.openLocationSettings();
-        // Permissions are denied forever, handle appropriately.
-        return Future.error(
-            'Location permissions are permanently denied, we cannot request permissions.');
-      }
-
-      if (permission == LocationPermission.denied) {
-        return Future.error('Location permissions are denied');
-      }
-    }
-    final position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
-    // Position position = await Geolocator.getLastKnownPosition();
-    print('position: $position}');
-    return position;
   }
 }
